1ec2195e3bc43380eb54e48e8e61a9094ceb227e
HP-SI-2L7KJIG\Sebi <ichimv@hotmail.com>
2018-11-27 07:25:03 +0200

    Lab 7 simple

--- Lab7 - Iluminare Phong simpla/PhongLight.fs
+++ Lab7 - Iluminare Phong simpla/PhongLight.fs
@@ -11,15 +11,12 @@ uniform vec3 objectColor;
 
 void main()
 {
-	// simple color blending
-    //FragColor = vec4(lightColor * objectColor, 1.0);
-	
-	// ambient
+    // ambient
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
   	
-    // diffuse
-	float diffuseStrength = 0.5;
+    // diffuse 
+    float diffuseStrength = 0.5;
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
@@ -29,7 +26,7 @@ void main()
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16);
     vec3 specular = specularStrength * spec * lightColor;  
         
     vec3 result = (ambient + diffuse + specular) * objectColor;
